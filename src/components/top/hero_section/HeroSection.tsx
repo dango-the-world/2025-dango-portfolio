cc444020c6a9464aa7aa4d58809e3c74c89dfa35
dango-the-world <z.rohaso@gmail.com>
2025-02-20 13:25:57 +0900

    feat: framer-motionを追加し、ヒーローセクションにアニメーション効果を適用

--- src/components/top/hero_section/HeroSection.tsx
+++ src/components/top/hero_section/HeroSection.tsx
@@ -1,18 +1,22 @@
+"use client";
+
 import { HeroBackground } from "@/components/background/HeroBackground";
 import { Box } from "@yamada-ui/layouts";
 import { Image } from "@yamada-ui/image";
+import { motion } from "framer-motion";
 import React from "react";
 
 export const HeroSection = () => {
   return (
     <HeroBackground>
-      <Box
-        h={"100vh"}
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Image alt="ロゴ" src="logo.png" w={"600px"} />
+      <Box h="100vh" display="flex" justifyContent="center" alignItems="center">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <Image alt="ロゴ" src="logo.png" w="600px" />
+        </motion.div>
       </Box>
     </HeroBackground>
   );
